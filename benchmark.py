7bf3d71e2ff2b82f18976b7531002335c63c5294
jose <joseantoniogarcia@ctnaval.com>
2026-04-21 11:09:51 +0200

    added support for different models from the hub via a --model flag from the cli

--- benchmark.py
+++ benchmark.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 
 from sentence_transformers import SentenceTransformer
 import numpy as np
@@ -13,8 +14,21 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--path", type=str, help="Path to eval set", required=True)
     parser.add_argument("-w", "--weights", type=str, help="The path to model weights", required=False)
     parser.add_argument("-k", "--k", type=int, help="top k for metrics computation", required=True)
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        help="the model to benchmark (if no weights are provided) (Alibaba-NLP/gte-modernbert-base or joe32140/ModernBERT-base-msmarco)",
+        required=False
+        )
     
     args = parser.parse_args()
+    
+    # basic logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
     
     # load horizon projects
     projects = load_projects("data", mono=True)
@@ -22,14 +36,24 @@ if __name__ == "__main__":
     # load the evaluation set
     dataset = load_eval_set(path_set=args.path)
     
-    # if not fine tuned we benchmark the gte base which is suited for retrieval
-    if not args.weights:
-        model = SentenceTransformer("Alibaba-NLP/gte-modernbert-base")
-        model.save("models/gte-modernbert-base")
-        # model = SentenceTransformer("joe32140/ModernBERT-base-msmarco")
-        # model.save("models/ModernBERT-base-msmarco")
-    else:
+    # check what we want to finetune
+    if not args.weights and args.model:
+        logging.info(f"Benchmarking model {args.model}")
+        model = SentenceTransformer(args.model)
+        name_model = args.model.split('/')[-1]
+        model.save(f"models/{name_model}")
+        
+    elif args.weights and not args.model:
+        logging.info(f"Benchmarking our finetuned model {args.weights}")
         model = SentenceTransformer(args.weights)
+    
+    elif args.weights and args.model:
+        logging.error("You must not set a custom model and a model from the hub.")
+        raise Exception()
+        
+    else:
+        logging.error("You need to set --weights or --model")
+        raise Exception()
     
     documents = list(projects["objective"])
     doc_ids = list(projects["id"])
